9cfa98291c45e34442c4fcd3b3c704f1777f5752
vaibhav <vaibhavb2016>
2024-06-12 07:17:16 +0000

    today2

---
+++ test/test.js
@@ -0,0 +1,14 @@
+// test/example.test.js
+const { expect } = require('chai');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      expect([1, 2, 3].indexOf(4)).to.equal(-1);
+    });
+
+    it('should return the correct index when the value is present', function() {
+      expect([1, 2, 3].indexOf(3)).to.equal(2);
+    });
+  });
+});
